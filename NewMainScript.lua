8146fdb0119a8702319b1af0f43dac17b2740d42
Fahri <117529823+Sidhsksjsjsh@users.noreply.github.com>
2024-08-30 16:39:03 +0900

    Update NewMainScript.lua

--- NewMainScript.lua
+++ NewMainScript.lua
@@ -103,6 +103,8 @@ elseif game.PlaceId == 17483941801 then
 	return lib:LoadRepository("https://raw.githubusercontent.com/Sidhsksjsjsh/Elite-Script-Folder/main/Rob-The-Place-Module.lua",true)
 elseif game.PlaceId == 15501353806 then
 	return lib:LoadRepository("https://raw.githubusercontent.com/Sidhsksjsjsh/Elite-Script-Folder/main/Swordmaster-Simulator-Module.lua",true)
+elseif game.PlaceId == 16677333114 then
+	return lib:LoadRepository("https://raw.githubusercontent.com/Sidhsksjsjsh/Elite-Script-Folder/main/Shoot-A-Friend.lua",true)
 else
 	return loadstring(game:HttpGet("https://raw.githubusercontent.com/Sidhsksjsjsh/VapeV4ForRoblox/main/MainScript.lua"))() --loadstring(vapeGithubRequest("MainScript.lua"))()
 end
