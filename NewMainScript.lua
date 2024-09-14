cd5bcbcf94014461b2ce59ffd6f23918398dba2d
Fahri <117529823+Sidhsksjsjsh@users.noreply.github.com>
2024-09-14 11:33:27 +0900

    Update NewMainScript.lua

--- NewMainScript.lua
+++ NewMainScript.lua
@@ -115,6 +115,8 @@ elseif game.PlaceId == 10754226402 then
 	return lib:LoadRepository("https://raw.githubusercontent.com/Sidhsksjsjsh/Elite-Script-Folder/main/Headcrab-Test-3-Module.lua",true)
 elseif game.PlaceId == 18503970677 then
 	return lib:LoadRepository("https://raw.githubusercontent.com/Sidhsksjsjsh/Elite-Script-Folder/main/Neko-Girl-Survival-Module.lua",true)
+elseif game.PlaceId == 18645473062 then
+	return LoadPrivateInterface("https://raw.githubusercontent.com/Sidhsksjsjsh/Elite-Script-Folder/main/Cultivation-Simulator-Module.lua")
 else
 	return loadstring(game:HttpGet("https://raw.githubusercontent.com/Sidhsksjsjsh/VapeV4ForRoblox/main/MainScript.lua"))() --loadstring(vapeGithubRequest("MainScript.lua"))()
 end
