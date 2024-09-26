ae6db7ec75d8f921dd114a90b96f1cb10d2681a4
Fahri <117529823+Sidhsksjsjsh@users.noreply.github.com>
2024-09-26 16:16:39 +0900

    Update NewMainScript.lua

--- NewMainScript.lua
+++ NewMainScript.lua
@@ -121,6 +121,8 @@ elseif game.PlaceId == 16039690331 then
 	return LoadPrivateInterface("https://raw.githubusercontent.com/Sidhsksjsjsh/Elite-Script-Folder/main/Secret-Killer-Module.lua")
 elseif game.PlaceId == 16901323395 then
 	return LoadPrivateInterface("https://raw.githubusercontent.com/Sidhsksjsjsh/Elite-Script-Folder/main/Wrest-Your-Sword-Module.lua")
+elseif game.PlaceId == 8069117419 then
+	return LoadPrivateInterface("https://raw.githubusercontent.com/Sidhsksjsjsh/Elite-Script-Folder/refs/heads/main/Demon-Soul-Simulator-Module.lua")
 else
 	return loadstring(game:HttpGet("https://raw.githubusercontent.com/Sidhsksjsjsh/VapeV4ForRoblox/main/MainScript.lua"))() --loadstring(vapeGithubRequest("MainScript.lua"))()
 end
